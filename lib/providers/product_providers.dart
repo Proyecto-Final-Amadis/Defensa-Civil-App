e558a3cabb265281c6c8d6cfe75934a038521171
Jeri-Cdk <jgomez@codika.net>
2025-03-31 17:51:01 -0400

    Refactor product providers to streamline service instantiation and product retrieval

--- lib/providers/product_providers.dart
+++ lib/providers/product_providers.dart
@@ -2,11 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:proyecto_final/models/product_model.dart';
 import 'package:proyecto_final/services/product_services.dart';
 
-// Instancia del servicio
 final productServiceProvider =
     Provider<ProductServices>((ref) => ProductServices());
 
-// FutureProvider para obtener los productos
 final productsProvider = FutureProvider<List<ProductModel>>((ref) async {
   final productService = ref.read(productServiceProvider);
   return await productService.getProducts();
